560fe09dd7f2bd121abede673ab83b9fdc0c72b1
Abdurrosyid Handoyo <arrosyidbh@gmail.com>
2013-03-22 04:43:21 +0700

    added main Encrypt and Decrypt Cipheruwet and Feistel method

---
+++ BlockCipheruwet.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cipheruwet
+{
+    class BlockCipheruwet
+    {
+        public BlockCipheruwet(byte[] input, byte[] key)
+        {
+            input.CopyTo(Input, 0);
+            key.CopyTo(Key, 0);
+
+            int i = 0;
+            for (; i < SIZE7; i++)
+            {
+                CipherA[i] = Input[i];
+            }
+            for (int k = 0; i < SIZE8; i++, k++)
+            {
+                CipherB[k] = Input[i];
+            }
+        }
+
+        public byte[] encrypt()
+        {
+            return encryptFeistel();
+        }
+
+        private byte[] encryptFeistel()
+        {
+            byte[] tempCipherA = new byte[SIZE7];
+            byte[] tempCipherB = new byte[SIZE7];
+            for (int i = 0; i < FEISTEL_COUNT; i++)
+            {
+                CipherB.CopyTo(tempCipherA, 0);
+                tempCipherB = bitwiseXOR(CipherB, new BlockCipheruwetEnDc(Key, CipherA).encrypt());
+
+                tempCipherA.CopyTo(CipherA, 0);
+                tempCipherB.CopyTo(CipherB, 0);
+            }
+            byte[] ret = new byte[SIZE8];
+            CipherA.CopyTo(ret, 0);
+            CipherB.CopyTo(ret, SIZE7);
+            return ret;
+        }
+
+        private byte[] bitwiseXOR(byte[] A, byte[] B)
+        {
+            byte[] ret = new byte[A.Length];
+            for (int i = 0; i < A.Length; i++)
+            {
+                ret[i] = (byte) (A[i] ^ B[i]);
+            }
+            return ret;
+        }
+
+        private byte[] Input;
+        private byte[] Key;
+        private byte[] CipherA;
+        private byte[] CipherB;
+
+        public static const int SIZE8 = (1 << 8);
+        public static const int SIZE7 = (1 << 7);
+        public static const int FEISTEL_COUNT = (1 << 4);
+    }
+}
---
+++ BlockCipheruwetEnDc.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cipheruwet
+{
+    class BlockCipheruwetEnDc
+    {
+        public BlockCipheruwetEnDc(byte[] key, byte[] input)
+        {
+            input.CopyTo(Input, 0);
+            key.CopyTo(Key, 0);
+            Table = new byte[SIZE];
+            generateRandom(sumKeys());
+        }
+
+        public byte[] encrypt()
+        {
+            return encryptTransposition(Input);
+        }
+
+        public byte[] decrypt()
+        {
+            return decryptTransposition(Input);
+        }
+
+        private byte[] decryptTransposition(byte[] input)
+        {
+            byte[] temp = reverseKey(input);
+            byte[] cipher = new byte[SIZE];
+
+            for (int i = 0; i < SIZE; i++)
+            {
+                cipher[Table[i]] = temp[i];
+            }
+
+            return cipher;
+        }
+
+        private byte[] encryptTransposition(byte[] input)
+        {
+            byte[] temp = reverseKey(input);
+            byte[] cipher = new byte[SIZE];
+
+            for (int i = 0; i < SIZE; i++)
+            {
+                cipher[i] = temp[Table[i]];
+            }
+
+            return cipher;
+        }
+
+        private byte[] reverseKey(byte[] input)
+        {
+            byte[] cipher = new byte[SIZE];
+            byte[] key = new byte[SIZE];
+            input.CopyTo(cipher, 0);
+            for (int i = 0; i < 8; i++)
+			{
+                cipher = keyXOR(cipher);
+                Key.Reverse();
+			}
+            return cipher;
+        }
+
+        private byte[] keyXOR(byte[] input)
+        {
+            byte[] cipher = new byte[SIZE];
+            for (int i = 0; i < SIZE; i++)
+            {
+                cipher[i] = (byte) (input[i] ^ Key[i]);
+            }
+            return cipher;
+        }
+
+        private void generateRandom(int key)
+        {
+            Random r = new Random(key);
+            for (int i = 0; i < SIZE; i++)
+            {
+                Table[i] = 0;
+            }
+
+            for (int i = 1; i < SIZE; i++)
+            {
+                int x = r.Next(SIZE);
+                while (Table[x] != 0)
+                {
+                    x = (x + 1) % SIZE;
+                }
+                Table[x] = (byte) i;
+            }
+        }
+
+        private int sumKeys()
+        {
+            int x = 0;
+            for (int i = 0; i < SIZE; i++)
+            {
+                x = (x + Key[i]) % (1 << 16);
+            }
+            return x;
+        }
+
+        private byte[] Key;
+        private byte[] Input;
+
+        private byte[] Table;
+
+        public static const int SIZE = (1 << 7);
+    }
+}
