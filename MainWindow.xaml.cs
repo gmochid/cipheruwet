441cc637a94b500c94fc6f80b2ad61b66dda4e27
Andhika Nugraha <andhika.nugraha@gmail.com>
2013-03-21 18:09:38 +0700

    Updated MainWindow.xaml
    Added browse for file selectors

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace cipheruwet
 {
@@ -23,6 +25,74 @@ namespace cipheruwet
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // --- OPEN FILE UI ---
+
+        private void summonOpenDialog(TextBox target)
+        {
+            // Instantiate open file dialog
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.FileName = "";
+            dlg.DefaultExt = "";
+            dlg.Filter = "All files (*.*)|*.*";
+
+            // Show dialog
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process choice
+            if (result == true)
+            {
+                string filename = dlg.FileName;
+                target.Text = filename;
+            }
+        }
+
+        private void browseUnencryptedFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            summonOpenDialog(unencryptedFileName);
+        }
+
+        private void browseEncryptedFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            summonOpenDialog(encryptedFileName);
+        }
+
+        // --- SAVE FILE UI ---
+
+        private void summonSaveDialog(TextBox target, TextBox source = null)
+        {
+            // Source file name
+            String sourceDirectoryName = System.IO.Path.GetDirectoryName(source.Text);
+            String sourceFileNameWithoutExtension = System.IO.Path.GetFileNameWithoutExtension(source.Text);
+            String sourceExtension = System.IO.Path.GetExtension(source.Text);
+
+            // Instantiate save file dialog
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.InitialDirectory = sourceDirectoryName;
+            dlg.FileName = "";
+            dlg.DefaultExt = sourceExtension;
+            dlg.Filter = "All files (*.*)|*.*";
+
+            // Show dialog
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process choice
+            if (result == true)
+            {
+                string filename = dlg.FileName;
+                target.Text = filename;
+            }
+        }
+
+        private void browseEncryptionDestinationFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            summonSaveDialog(encryptionDestinationFileName, unencryptedFileName);
+        }
+
+        private void browseDecryptionDestinationFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            summonSaveDialog(decryptionDestinationFileName, encryptedFileName);
         }
     }
 }
