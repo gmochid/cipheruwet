9c5c6d04a89736224d2ff3dcfcc770b49bc04e9f
Andhika Nugraha <andhika.nugraha@gmail.com>
2013-03-21 18:52:01 +0700

    Added Engine.cs

---
+++ Engine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cipheruwet
+{
+    class Engine
+    {
+        public static void startEncryption(String sourceFileName, String destinationFileName, String key, String mode)
+        {
+        }
+
+        public static void startDecryption(String sourceFileName, String destinationFileName, String key)
+        {
+            // Mode is read from the header of the file
+        }
+    }
+}
